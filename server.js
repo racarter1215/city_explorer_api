138222de4327f560dcc70f84ce9f8036648a57c9
racarter1215 <racarter1215@gmail.com>
2020-04-14 10:28:14 -0700

    added front end library

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/location', (request, response) => {
+
+}
+
+function City()
 app.listen(PORT, () => {
   console.log('Server is running on PORT: ' + PORT);
 });
