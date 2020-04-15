ab910254b9ca4ba6137728b33d648715eed67d23
racarter1215 <racarter1215@gmail.com>
2020-04-14 21:40:42 -0700

    fixed server completely

--- server.js
+++ server.js
@@ -28,34 +28,6 @@ function Location(geo, city) {
     this.longitude = geo.lon;
 }
 
-// function handleLocation(request, response) {
-//     const locationData = require('./data/geo.json');
-//     const results = [];
-//     const cityQuery = request.query.city;
-//     for (var i in locationData) {
-//         if (locationData[i].display_name.search(cityQuery)) {
-//             const location = new Location(cityQuery, locationData[i]);
-//             response.send(location);
-//             return;
-//         }
-//     }
-//     locationData.forEach(item => {
-//         results.push(new Location(item.geo, item.city));
-//     })
-//     response.send(results);
-// }
-
-// app.get('/location', (request, response) => {
-//     let city = request.query.city || 'Lynnwood';
-//     let geo = require('./data/geo.json');
-//     let location = new Location(geo[0], city)
-//     if (location) {
-//         response.status(200).send(location);
-//     } else {
-//         response.status(404).send('Cant find your city');
-//     } console.log(location);
-// });
-
 app.get('/weather', (request, response) => {
     let city = request.query.search_query;
     let formatted_query = request.query.formatted_query;
@@ -74,26 +46,9 @@ function Weather(obj) {
     this.time = new Date(obj.time * 1000).toDateString();
 }
 
-// function handleWeather(request, response) {
-//     const weatherData = require('./data/darksky.json').data;
-
-//     const results = [];
-//     weatherData.forEach(item => {
-//         results.push(new Weather(item.datetime, item.weather.description));
-//     })
-//     response.send(results);
-// }
-
 app.get('*',(request, response) => {
     response.status(404).send('Sorry, something is wrong');
 })
-
-
-// app.get('/location', handleLocation);
-// app.get('/weather', handleWeather);
-// app.use((request, response) => {
-//     handleError('You had an error', request, response);
-// });
 
 app.listen(PORT, () => {
   console.log('Server is running on PORT: ' + PORT);
