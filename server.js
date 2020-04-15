5fb555e3a20712466c1b6100a4d35c2ad04e283c
racarter1215 <racarter1215@gmail.com>
2020-04-14 19:26:03 -0700

    fixed server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const cors = require('cors');
-app.use(cors());
+
 
 function Location(geo, city) {
     this.search_query = city;
@@ -16,15 +16,61 @@ function Location(geo, city) {
     this.longitude = geo.lon;
 }
 
-app.get('/location', (request, response) => {
-    let city = request.query.city || 'Lynnwood';
-    let geo = require('./data/geo.json');
-    let location = new Location(geo[0], city)
-    if (location) {
-        response.status(200).send(location);
-    } else {
-        response.status(404).send('Cant find your city');
-    } console.log(location);
+function handleLocation(request, response) {
+    const locationData = require('./data/geo.json');
+    const results = [];
+    const cityQuery = request.query.city;
+    for (var i in locationData) {
+        if (locationData[i].display_name.search(cityQuery)) {
+            const location = new Location(cityQuery, locationData[i]);
+            response.send(location);
+            return;
+        }
+    }
+    locationData.forEach(item => {
+        results.push(new Location(item.geo, item.city));
+    })
+    response.send(results);
+}
+
+// app.get('/location', (request, response) => {
+//     let city = request.query.city || 'Lynnwood';
+//     let geo = require('./data/geo.json');
+//     let location = new Location(geo[0], city)
+//     if (location) {
+//         response.status(200).send(location);
+//     } else {
+//         response.status(404).send('Cant find your city');
+//     } console.log(location);
+// });
+
+function Forecast(forecast, date) {
+    this.forecast = forecast;
+    this.date = new Date(date);
+}
+
+function handleWeather(request, response) {
+    const weatherData = require('./data/darksky.json').data;
+
+    const results = [];
+    weatherData.forEach(item => {
+        results.push(new Forecast(item.datetime, item.weather.description));
+    })
+    response.send(results);
+}
+
+function handleError(error, request, response) {
+    response.send(500).send({
+    status: 500,
+    responseText: "Sorry, something went wrong",
+    });
+}
+
+app.use(cors());
+app.get('/location', handleLocation);
+app.get('/weather', handleWeather);
+app.use((request, response) => {
+    handleError('You had an error', request, response);
 });
 
 app.listen(PORT, () => {
