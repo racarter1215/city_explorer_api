10ed643a6451f4d72e4bdd54a346da975d360d12
racarter1215 <racarter1215@gmail.com>
2020-04-16 11:30:38 -0700

    derp

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 const express = require('express');
 const superagent = require('superagent');
-const pg = require('pg');
+// const pg = require('pg');
 const cors = require('cors');
 const app = express();
-const dbClient = new pg.Client(process.env.DATABASE_URL);
+// const dbClient = new pg.Client(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 
@@ -95,7 +95,7 @@ function handleError(error, request, response) {
     response.status(500).send({status: 500, responseText: 'Sorry something went wrong'});
   }
 
-app.use('*', (request, response) => response.send('Sorry that route does not exist'));
+app.use('/', (request, response) => response.send('Sorry that route does not exist'));
 
 app.listen(PORT, () => {
   console.log('Server is running on PORT: ' + PORT);
